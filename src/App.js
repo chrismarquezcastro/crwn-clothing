2a8a15a263b66cb2efb841e5c63e9bfd339ecb4d
Chris Marquez <ixdchrismarquez@gmail.com>
2022-04-17 20:24:01 -0700

    Build directory component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,35 @@
-import "./categories.scss";
+import Directory from "./components/Directory/Directory";
 
 const App = () => {
   const categories = [
     {
       id: 1,
-      title: "Hats",
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
     },
     {
       id: 2,
-      title: "Jackets",
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
     },
     {
       id: 3,
-      title: "Sneakers",
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
     },
     {
       id: 4,
-      title: "Womens",
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     },
     {
       id: 5,
-      title: "Mens",
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
   ];
 
-  return (
-    <div className="categoriesContainer">
-      {categories.map(({ title }) => (
-        <div className="categoryContainer">
-          <div className="backgroundImage" />
-          <div className="categoryBodyContainer">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+  return <Directory categories={categories} />;
 };
 
 export default App;
